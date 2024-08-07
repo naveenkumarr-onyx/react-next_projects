94e68223a7c04cce095c054bb23c84d72ddd478c
Naveenkumar <121660045+naveenkumarr-onyx@users.noreply.github.com>
2024-08-07 16:57:04 +0530

    created oldStudentjson

--- app/components/tanstackQuery/Query.tsx
+++ app/components/tanstackQuery/Query.tsx
@@ -67,3 +67,25 @@ const ReactQuery = () => {
 };
 
 export default ReactQuery;
+
+//usage......!!!!!
+
+// "use client";
+// import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+// import ReactQuery from "./components/tanstackQuery/Query";
+// import { useState } from "react";
+// import RenderHtml from "./components/htmlparser/RenderHtml";
+
+// export default function Home() {
+//   const [client] = useState(new QueryClient());
+//   return (
+//     <main>
+//       <div>
+//         <QueryClientProvider client={client}>
+//           <ReactQuery />
+//           <RenderHtml />
+//         </QueryClientProvider>
+//       </div>
+//     </main>
+//   );
+// }
