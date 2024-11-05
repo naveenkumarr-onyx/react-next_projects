e0da18e5a91c31086c4ccb17dadefe5dd7caef19
Naveenkumar <121660045+naveenkumarr-onyx@users.noreply.github.com>
2024-11-05 14:50:52 +0530

    installed/mongodb,mongoose

--- node_backend/index.js
+++ node_backend/index.js
@@ -1,13 +1,19 @@
-"strict mode";
-const http = require("http");
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import { connectDb } from "./config/db.js";
+import user from "./routes/userRoutes.js";
 
-const args = process.argv; //+
-console.log(args);
+const app = express();
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+dotenv.config();
+app.use(express.json());
+mongoose.connect(process.env.MONGODB_URI);
+connectDb();
+app.use("/user", user);
 
-http
-  .createServer((req, res) => {
-    res.write("Hello");
-    res.end();
-    console.log("Server is running on port 3000");
-  })
-  .listen(5000);
+app.listen(process.env.PORT, () => {
+  console.log("Server is running on port 5000");
+});
