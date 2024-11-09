284a98c1159c1254a166d8e7c458bdc905f46cb9
Naveenkumar <121660045+naveenkumarr-onyx@users.noreply.github.com>
2024-11-09 17:12:43 +0530

    create/logincontroller

--- node_backend/controllers/usersController.js
+++ node_backend/controllers/usersController.js
@@ -38,3 +38,30 @@ export const signupController = async (req, res) => {
     });
   }
 };
+
+export const loginController = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const existingUser = await Signup.findOne({ email });
+    if (!existingUser) {
+      return res.status(404).json({ message: "User not found." });
+    }
+    const isMatch = await bcrypt.compare(password, existingUser.password);
+    if (!isMatch) {
+      return res
+        .status(400)
+        .json({ message: "Invalid credentials.", success: false });
+    } else {
+      return res.status(200).json({
+        message: "Logged in successfully",
+        success: true,
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: "Server error. Please try again later.",
+      error: error.message,
+      success: false,
+    });
+  }
+};
--- node_backend/routes/userRoutes.js
+++ node_backend/routes/userRoutes.js
@@ -1,7 +1,11 @@
 import express from "express";
-import { signupController } from "../controllers/usersController.js";
+import {
+  loginController,
+  signupController,
+} from "../controllers/usersController.js";
 
 const router = express.Router();
 router.post("/signup", signupController);
+router.post("/login", loginController);
 
 export default router;
